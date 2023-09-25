e7a1686e4dd3b8e93be3fd1e8b066d19106f5466
Zeliha Arslan <72877610+Zelihaars@users.noreply.github.com>
2023-09-25 22:31:02 +0300

    binary sort task complete

---
+++ task3.py
@@ -0,0 +1,70 @@
+# Dizi: [7, 5, 1, 8, 3, 6, 0, 9, 4, 2]
+
+# İlk eleman olan 7'yi kök (root) olarak ekleyelim:
+
+
+#      7
+# 5, 1 ve 8'i ekleyelim (sırayla):
+
+
+#      7
+#     / \
+#    5   8
+#   /
+#  1
+# 3'ü ekleyelim:
+
+
+#      7
+#     / \
+#    5   8
+#   / \
+#  1   3
+# 6'yı ekleyelim:
+
+
+#      7
+#     / \
+#    5   8
+#   / \
+#  1   3
+#     /
+#    6
+# 0'ı ekleyelim:
+
+
+#      7
+#     / \
+#    5   8
+#   / \
+#  1   3
+#   \
+#    6
+#   /
+#  0
+# 9'u ekleyelim:
+
+
+#      7
+#     / \
+#    5   8
+#   / \   \
+#  1   3   9
+#   \
+#    6
+#   /
+#  0
+# Son olarak, 4 ve 2'yi ekleyelim (sırayla):
+
+
+#      7
+#     / \
+#    5   8
+#   / \   \
+#  1   3   9
+#   \
+#    6
+#   / \
+#  0   4
+#       \
+#        2
