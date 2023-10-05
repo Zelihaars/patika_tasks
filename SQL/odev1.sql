4a63ee3900d2b5f0c4bebcc439e991e3dff75843
Zeliha Arslan <72877610+Zelihaars@users.noreply.github.com>
2023-10-05 10:48:42 +0200

    Add files via upload

---
+++ SQL/odev1.sql
@@ -0,0 +1,6 @@
+ 
+-- 1) SELECT title, description from film;
+-- 2) SELECT * from film WHERE length>60 AND length <75 ;
+-- 3) SELECT * from film WHERE rental_rate= 0.99 AND replacement_cost=12.99 OR replacement_cost=28.99
+-- 4) SELECT first_name, last_name from customer WHERE first_name='Mary' Yani cevap Smith
+-- 5) SELECT * FROM film WHERE NOT length>50 AND NOT (rental_rate=2.99 OR rental_rate=4.99);
