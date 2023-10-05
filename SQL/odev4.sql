edca38882e661f04665873718ba7b33f3cdcd69a
Zeliha Arslan <72877610+Zelihaars@users.noreply.github.com>
2023-10-05 17:19:34 +0200

    Add files via upload

---
+++ SQL/odev4.sql
@@ -0,0 +1,6 @@
+
+-- 1)SELECT DISTINCT replacement_cost FROM film
+-- 2)SELECT COUNT (DISTINCT replacement_cost) FROM film
+-- 3)SELECT COUNT(title) FROM film WHERE title LIKE 'T%' AND rating='G'
+-- 4)SELECT COUNT (DISTINCT country) FROM country WHERE country LIKE '_____';
+-- 5)SELECT COUNT (DISTINCT city) FROM city WHERE city ILIKE 'R%'
