5e0675476a7c45993267218690a4ae2fccb40b75
Zeliha Arslan <72877610+Zelihaars@users.noreply.github.com>
2023-10-05 16:54:17 +0200

    Add files via upload

---
+++ SQL/odev3.sql
@@ -0,0 +1,6 @@
+
+
+-- 1)SELECT country FROM country WHERE country LIKE 'A%a';
+-- 2)SELECT country FROM country WHERE country LIKE '_____%n';
+-- 3)SELECT title FROM film WHERE title ILIKE '%t%t%t%t%'
+-- 4)SELECT * FROM film WHERE title LIKE 'C%' AND length >90 AND rental_rate =2.99
