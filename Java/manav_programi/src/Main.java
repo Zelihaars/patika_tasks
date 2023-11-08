6eba54d4759bbc640ffdae63fb596b86dd441ce4
Zeliha Arslan <72877610+Zelihaars@users.noreply.github.com>
2023-11-08 15:02:22 +0100

    manav programı

--- Java/manav_programi/src/Main.java
+++ Java/manav_programi/src/Main.java
@@ -1,17 +1,40 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+      double armut, armutKg=2.14;
+      double elma, elmaKg=3.67;
+      double domates, domatesKg=1.11;
+      double muz, muzKg=0.95;
+      double patlican, patlicanKg=5.00;
+      Scanner scan = new Scanner(System.in);
+      
+      System.out.print("Armut kaç kilo ? : ");
+      armut=scan.nextInt();
+      armutKg=armutKg*armut;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+      System.out.print("Elma kaç kilo ? : ");
+      elma=scan.nextInt();
+      elmaKg=elmaKg*elma;
+    
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+      System.out.print("Domates kaç kilo ? : ");
+      domates=scan.nextInt();
+      domatesKg=domatesKg*domates;
+
+      System.out.print("Muz kaç kilo ? : ");
+      muz=scan.nextInt();
+      muzKg=muzKg*muz;
+
+      System.out.print("Patlıcan kaç kilo ? : ");
+      patlican=scan.nextInt();
+      patlicanKg=patlicanKg*patlican;
+
+      double toplam=armutKg+elmaKg+domatesKg+muzKg+patlicanKg;
+
+      System.out.print("Toplam Tutar: "+toplam);
+      
+    
+    
     }
 }
