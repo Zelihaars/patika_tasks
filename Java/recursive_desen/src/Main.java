c70fb7e6ee228c6cdb7710c0dc89a2ac05a6a508
Zeliha Arslan <72877610+Zelihaars@users.noreply.github.com>
2023-11-14 10:10:30 +0100

    Recursive desen

---
+++ Java/recursive_desen/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        int num = scanner.nextInt();
+        desen(num);
+    }
+
+    public static void desen(int num) {
+        System.out.print(num + " ");
+        if (num <= 0) {
+            return;
+        }
+        desen(num - 5);
+        System.out.print(num + " ");
+    }
+}
