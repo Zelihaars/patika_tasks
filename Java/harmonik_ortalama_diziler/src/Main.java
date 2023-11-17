2d82d8c13c75d7bd2c3207a36f41182622941f82
Zeliha Arslan <72877610+Zelihaars@users.noreply.github.com>
2023-11-17 15:00:01 +0100

    Harmonik Ortalama

---
+++ Java/harmonik_ortalama_diziler/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        double sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum += (1.0/numbers[i]);
+        }
+
+        System.out.println("Harmonik seri: " + sum);
+        System.out.println("Ortalama: " + numbers.length/sum);
+
+    }
+}
